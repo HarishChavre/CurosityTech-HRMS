94d37cb04ffb2c2ce1722066679caf0edb018077
HarishChavre <harrych_1@yahoo.com>
2025-05-06 12:37:28 +0530

    backend setup done

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('HRMS Backend is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
